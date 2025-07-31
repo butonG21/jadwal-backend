9f6226af70b95203d6917166abce060e55ee2986
butong <anggi21.af@gmail.com>
2025-08-01 02:50:37 +0700

    add auth for filter endpoint

--- routes/scheduleRoutes.ts
+++ routes/scheduleRoutes.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import multer from 'multer';
 import { uploadExcel, getAllSchedules, getSchedulesByEmployee } from '../controllers/scheduleController';
+import { verifyToken } from '../middlewares/verifyToken';
+
 
 const router = express.Router();
 const upload = multer(); // pakai memory storage (tidak simpan ke disk)
 
 router.post('/upload-excel', upload.single('file'), uploadExcel);
 router.get('/all', getAllSchedules);
-router.get('/filter', getSchedulesByEmployee);
+router.get('/filter', verifyToken,getSchedulesByEmployee);
 
 
 export default router;
